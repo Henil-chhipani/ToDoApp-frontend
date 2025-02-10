253542d5eea7571e2fc1cc4c1f1862c6fb726522
Henil Chhipani <henilchhipani@gmail.com>
2025-02-10 15:47:44 +0530

    new todo app frontend in react

---
+++ src/components/addTaskForm.jsx
@@ -0,0 +1,144 @@
+import { Container, TextField, Stack, Button } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+
+export default function AddTaskForm({ taskData, onTaskAdded }) {
+    const [task, setTask] = useState({
+        taskTitle: "",
+        taskDescription: ""
+    })
+    let id, value
+    const handleOnChange = (e) => {
+        id = e.target.id
+        value = e.target.value
+        setTask({ ...task, [id]: value })
+    }
+
+
+
+    useEffect(() => {
+        if (taskData) {
+            setTask({
+                taskTitle: taskData.taskTitle,
+                taskDescription: taskData.taskDescription,
+            });
+        } else {
+            // Clear the form for adding a new task
+            setTask({
+                taskTitle: "",
+                taskDescription: ""
+            });
+        }
+    }, [taskData]);
+
+
+    const isEmpty = () => {
+        for (const key in task) {
+            if (task[key] === "") {
+                return true;
+            }
+        }
+        return false;
+    };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (isEmpty()) {
+            const emptyField = Object.keys(task).find((key) => task[key] === "");
+            document.getElementById(emptyField)?.focus();
+            return;
+        }
+
+        try {
+            const response = await fetch("http://localhost:3001/api/v1/tasks/addingTask",
+                {
+                    method: "POST",
+                    body: JSON.stringify(task),
+                    headers: {
+                        "Content-Type": "application/json",
+
+                    }
+                }
+            )
+                .then((res) => res.json())
+                .then((response) => {
+                    setTask({
+                        taskTitle: "",
+                        taskDescription: ""
+                    })
+                    onTaskAdded()
+                })
+
+
+            console.log("response:", response.status);
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            else {
+                console.log("task added");
+
+            }
+
+
+        } catch (error) {
+            console.error("Fetch error:", error);
+        }
+    }
+
+    const handleUpdate = async (e) => {
+        e.preventDefault();
+      
+        if (isEmpty()) {
+          const emptyField = Object.keys(task).find(
+            (key) => task[key].trim() === ""
+          );
+          document.getElementById(emptyField)?.focus();
+          return;
+        }
+      
+        const data = {
+          _id: taskData._id,
+          taskTitle: task.taskTitle,
+          taskDescription: task.taskDescription,
+        };
+
+        console.log("data",data)
+      
+        try {
+          const res = await fetch("http://localhost:3001/api/v1/tasks/updateTask", {
+            method: "PUT",
+            body: JSON.stringify(data),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+      
+          
+          const responseData = await res.json();
+          console.log("Update response:", responseData);
+      
+          
+          setTask({
+            taskTitle: "",
+            taskDescription: "",
+          });
+      
+          
+          onTaskAdded();
+        } catch (error) {
+          console.error("Fetch error:", error);
+        }
+      };
+      
+
+
+    return (
+        <Stack sx={{ padding: "10px", display: "flex", flexDirection: "column", width: "50%" }} spacing={2}>
+            <TextField variant="outlined" label="Add task title" id='taskTitle' name='taskTitle' onChange={handleOnChange} value={task.taskTitle} />
+            <TextField variant="outlined" label="Add task descrpiption" id='taskDescription' name='taskDescription' value={task.taskDescription} onChange={handleOnChange} />
+
+            <Button variant='outlined' onClick={taskData ? handleUpdate : handleSubmit}>
+                {taskData ? "Update Task" : "Add task"}
+            </Button>
+        </Stack>
+
+    )
+}
---
+++ src/pages/home.jsx
@@ -0,0 +1,140 @@
+
+import { AppBar, Button, Grid2, Container, Card, List, ListItem, ListItemText, IconButton, Checkbox  } from "@mui/material"
+import TopBar from "../components/TopBar"
+import AddTaskForm from "../components/addTaskForm"
+import { useEffect, useState } from "react"
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit'
+
+export default function HomePage() {
+
+    const [tasks, setTasks] = useState([])
+    const [isFormVisible, setFormVisble] = useState(false)
+    const [taskToEdit, setTaskToEdit] = useState(null);
+
+
+    useEffect(() => {
+        fetch("http://localhost:3001/api/v1/tasks/getTasks")
+            .then((res) => res.json())
+            .then((response) => {
+                setTasks(response.data)
+                console.log(response.data);
+
+            })
+            .catch((error) => console.error("error in fetching task", error))
+    }, [])
+
+    const fetchTasks = () => {
+        fetch("http://localhost:3001/api/v1/tasks/getTasks")
+            .then((res) => res.json())
+            .then((response) => {
+                setTasks(response.data)
+                console.log(response.data);
+
+            })
+            .catch((error) => console.error("error in fetching task", error))
+    }
+
+    const toggleVisblity = (val) => {
+        setFormVisble(val)
+        if (isFormVisible) {
+            setTaskToEdit(null);
+        }
+    }
+
+    const onDeletClick = (id) => {
+        fetch(`http://localhost:3001/api/v1/tasks/deleteTask/${id}`, {
+            method: 'DELETE',
+            headers: {
+                "Content-Type": "application/json",
+            }
+        })
+            .then((res) => res.json())
+            .then((response) => {
+                console.log("Task deleted:", response);
+                fetchTasks();
+            })
+            .catch((error) => console.error("Error deleting task:", error));
+
+    }
+
+    const onEditClick = (task) =>{
+       toggleVisblity(true)
+       setTaskToEdit(task);
+
+    }
+
+    const handleCheckboxChange = (id) => {
+
+        const task = tasks.find(t => t._id === id);
+        if (!task) return;
+
+        const updatedStatus = !task.isCompleted;
+        
+
+        fetch(`http://localhost:3001/api/v1/tasks/updateIsCompleted`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ _id: id, isCompleted: updatedStatus })
+        })
+          .then((res) => res.json())
+          .then((response) => {
+            console.log("Task updated:", response);
+        
+            fetchTasks();
+        
+          })
+          .catch((error) => console.error("Error updating task", error));
+      };
+      
+
+    return (
+        <div>
+            <TopBar />
+
+            <Container sx={{ paddingTop: "100px" }} >
+                <Button variant="outlined" onClick={() => toggleVisblity(!isFormVisible)}>Add Task</Button>
+                <div>
+                    {isFormVisible ? <AddTaskForm taskData={taskToEdit} onTaskAdded={fetchTasks} /> : null}
+                </div>
+
+
+                <List>
+                    {
+
+                        [...tasks].reverse().map((task) => {
+                            return (
+                                <ListItem key={task._id}
+                                    secondaryAction={
+                                        <div>
+                                            <IconButton edge="end" aria-label="delete" onClick={() => onEditClick(task)}>
+                                                <EditIcon />
+                                            </IconButton>
+                                            <IconButton edge="end" aria-label="delete" onClick={() => onDeletClick(task._id)}>
+                                                <DeleteIcon />
+                                            </IconButton>
+                                        </div>
+                                    }
+                                >
+                                    <Checkbox
+                                        edge="start"
+                                        checked={task.isCompleted}
+                                        onChange={() => handleCheckboxChange(task._id)}
+                                    />
+                                    <ListItemText
+                                        primary={task.taskTitle}
+                                        secondary={task.taskDescription}
+                                    />
+                                </ListItem>)
+                        })
+                    }
+                </List>
+
+            </Container>
+        </div>
+    )
+}
+
+
